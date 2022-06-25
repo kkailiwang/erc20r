245c79c4a15ce4940d32b525ea7cf35ed51f6508
kkailiwang <kailiwang2018@gmail.com>
2022-06-25 01:05:00 -0500

    add rinkeby configs back in

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,11 +8,11 @@ const { API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
   solidity: "0.8.1",
   // defaultNetwork: "rinkeby",
-  // networks: {
-  //   hardhat: {},
-  //   rinkeby: {
-  //     url: API_URL,
-  //     accounts: [`0x${PRIVATE_KEY}`]
-  //   }
-  // },
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  },
 };
