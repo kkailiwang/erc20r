af060b9dfb1bc844f34599c7a3af7917dcdbe8bd
kkailiwang <kailiwang2018@gmail.com>
2022-07-04 19:12:01 -0500

    format

--- scripts/clean721R.js
+++ scripts/clean721R.js
@@ -1,19 +1,19 @@
 //example script scraping all of the transaction data that must be cleaned for a given block era. 
 
-const { ethers } = require("hardhat");
+require("hardhat");
 
 const clean = async (ethersContract, currblock) => {
     const findTokenIdsToClean = async () => {
         const numReversibleBlocks = await ethersContract.NUM_REVERSIBLE_BLOCKS();
-        const transfers = await ethersContract.queryFilter('Transfer', 0, currblock - numReversibleBlocks );
-        const longerThan1 = async (tokenId) => {
-            console.log(tokenId, await ethersContract.owningGetLength(tokenId)>1);
-            return await ethersContract.owningGetLength(tokenId)>1;
+        const transfers = await ethersContract.queryFilter('Transfer', 0, currblock - numReversibleBlocks);
+        async (tokenId) => {
+            console.log(tokenId, await ethersContract.owningGetLength(tokenId) > 1);
+            return await ethersContract.owningGetLength(tokenId) > 1;
         }
         const unfilteredIds = Array(...new Set(transfers.map(transfer => transfer.args.tokenId)));
         let filteredIds = [];
-        for (let i=0; i < unfilteredIds.length; i++){
-            if ((await ethersContract.owningGetLength(unfilteredIds[i]))>1){
+        for (let i = 0; i < unfilteredIds.length; i++) {
+            if ((await ethersContract.owningGetLength(unfilteredIds[i])) > 1) {
                 filteredIds.push(i);
             }
         }
